03330e8c99a0ca20de96ea737f7ecc7296401198
Carlos Oliveira <carlosmoliveira93@gmail.com>
2024-06-22 13:41:21 -0300

    fix: manually acknowledge message

--- apps/billing/src/billing.controller.ts
+++ apps/billing/src/billing.controller.ts
@@ -1,13 +1,18 @@
 import { Controller } from '@nestjs/common';
 import { BillingService } from './billing.service';
 import { EventPattern, Payload, RmqContext } from '@nestjs/microservices';
+import { RmqService } from '@app/common';
 
 @Controller()
 export class BillingController {
-  constructor(private readonly billingService: BillingService) {}
+  constructor(
+    private readonly billingService: BillingService,
+    private readonly rmqService: RmqService,
+  ) {}
 
   @EventPattern('order_created')
   async handleOrderCreated(@Payload() data: any, context: RmqContext) {
     this.billingService.bill(data);
+    this.rmqService.ack(context);
   }
 }
